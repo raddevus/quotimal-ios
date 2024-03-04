c80d675322eac021ee47df057dcc0548482b0cd4
raddevus <1radeutsch1@gmail.com>
2024-03-04 16:28:09 -0500

    Initial Commit

---
+++ quotimal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quotimal
+//
+//  Created by roger deutsch on 3/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quotimal/quotimalApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotimalApp.swift
+//  quotimal
+//
+//  Created by roger deutsch on 3/4/24.
+//
+
+import SwiftUI
+
+@main
+struct quotimalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
