3d7870bfbdc57f0ed08612626c298caa423a6e8a
raddevus <1radeutsch1@gmail.com>
2024-03-04 16:52:15 -0500

    Got the basic layout set up. Loading an image using AsyncImage.

--- quotimal/ContentView.swift
+++ quotimal/ContentView.swift
@@ -10,7 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            AsyncImage(url: URL(string:"https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_1280.jpg"))
+            AsyncImage(url: URL(string:"https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_1280.jpg")){
+                phase in
+                if let image = phase.image{
+                    image
+                    .resizable()
+                    .scaledToFit()
+                }
+                else if phase.error != nil{
+                    Text("Error loading image.")
+                }else{
+                    ProgressView()
+                }
+            }
+            Text("This is the quote that you are look for.")
+                .font(.headline)
         }
         .padding()
     }
