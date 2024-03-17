be65276cf7c7be41cf6f22a76f2da27b72d933f4
raddevus <1radeutsch1@gmail.com>
2024-03-17 15:56:03 -0400

    Altered layout design toward final product.

--- quotimal/ContentView.swift
+++ quotimal/ContentView.swift
@@ -9,25 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string:"https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_1280.jpg")){
-                phase in
-                if let image = phase.image{
-                    image
-                    .resizable()
-                    .scaledToFit()
-                }
-                else if phase.error != nil{
-                    Text("Error loading image.")
-                }else{
-                    ProgressView()
-                }
+        ZStack{
+            Color(.systemGray)
+                .ignoresSafeArea()
+            
+            VStack (alignment: .leading, spacing:20.0   ){
+                AsyncImage(url: URL(string:"https://cdn.pixabay.com/photo/2016/11/14/04/45/elephant-1822636_1280.jpg")){
+                    phase in
+                    if let image = phase.image{
+                        image
+                        .resizable()
+                        .scaledToFit()
+                    }
+                    else if phase.error != nil{
+                        Text("Error loading image.")
+                    }else{
+                        ProgressView()
+                    }
+                }.cornerRadius(15)
+                    
+                .padding()
+                Text("This is the quote that you are looking for.")
+                    .font(.title)
+                    .padding()
+                    
             }
-            Text("This is the quote that you are look for.")
-                .font(.headline)
+            .background(Rectangle().foregroundColor(.white ))
+            
+            .cornerRadius(10)
+            .shadow(radius: 10 )
+            .padding()
         }
-        .padding()
-    }
+        }
+        
 }
 
 #Preview {
